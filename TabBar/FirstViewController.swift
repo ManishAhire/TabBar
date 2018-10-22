db82da1c100cde6e24e6f47726438834b167b30a
Manish Ahire <manish.ahire@msm.digital>
2018-10-22 17:20:26 +0530

    Initial Commit

---
+++ TabBar/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  TabBar
+//
+//  Created by Manish Ahire on 22/10/18.
+//  Copyright © 2018 Manish Ahire. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TabBar/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  TabBar
+//
+//  Created by Manish Ahire on 22/10/18.
+//  Copyright © 2018 Manish Ahire. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
